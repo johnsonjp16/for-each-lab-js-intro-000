74185e4b8e8a87fc8658a6f6220c267b76586dca
Jeff Johnson <johnsonjp@yahoo.com>
2018-12-28 18:34:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function iterativeLog(array){
-   array.forEach((element, index, array) => {
-     //debugger
-     console.log(`${index}: ${element}`)
-   })
- }
+  array.forEach((element, index, array) => {
+    debugger
+    console.log(`${index}: ${element}`)
+  })
+}
 
- function iterate(callback) {
-    var array = [123]
-    array.forEach(callback)
-    return array
-  }
-  
-   
-   function doToArray(array, callback) {
-    array.forEach(callback)	   
-  }	
+function iterate(callback) {
+  var array = [123]
+  array.forEach(callback)
+  return array
+}
+
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+}
